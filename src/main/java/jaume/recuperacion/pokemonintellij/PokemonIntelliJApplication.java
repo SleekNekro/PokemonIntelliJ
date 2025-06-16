2d06da8709d4a6b3ebd68c546f6b01be835e5e7a
portatil-V15 <ikerperezmata03@gamil.com>
2025-06-16 14:05:05 +0200

    Initial commit

---
+++ src/main/java/jaume/recuperacion/pokemonintellij/PokemonIntelliJApplication.java
@@ -0,0 +1,13 @@
+package jaume.recuperacion.pokemonintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonIntelliJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonIntelliJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jaume/recuperacion/pokemonintellij/PokemonIntelliJApplicationTests.java
@@ -0,0 +1,13 @@
+package jaume.recuperacion.pokemonintellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonIntelliJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
